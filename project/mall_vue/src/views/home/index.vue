36012cafae38c55e31cee9cdb8fef7616b7e497a
Paulliant <paulliantchen@gmail.com>
2024-04-25 00:25:03 +0800

    front end refined

--- project/mall_vue/src/views/home/index.vue
+++ project/mall_vue/src/views/home/index.vue
@@ -18,7 +18,7 @@
                 </el-col>
                 <el-col :span="9">
                   <div style="display: flex; align-items: center;">
-                  <label for="storeName" style="margin-right: 8px;"></label>
+                  <label for="storeName" style="margin-right: 8px;"> Location </label>
                   <el-input v-model="storeLocation" placeholder="Enter location"></el-input>
                   </div>
                 </el-col>
@@ -31,7 +31,7 @@
       </el-col>
       <el-col :span="4">
           <div class="name-layout">
-            <div class="layout-title">Avg Ratings</div>
+            <div class="layout-title">Avg Rate</div>
             <div class="content-box center-content">
               {{ this.avgRate }}
             </div>
@@ -43,22 +43,46 @@
     <!-- Feedbacks Section -->
     <div class="advice-layout">
       <div class="layout-title">Feedbacks</div>
-      <div class="content-box">
-        <div class="content-item" v-for="item in this.feedback" :key="item">
-            "{{ item }}"
-        </div>
-      </div>
+        <el-table
+          :data="feedback"
+          style="width: 100%"
+          border>
+          <el-table-column
+            prop="Comment"
+            label="Comment"
+            :width="commentWidth"
+            >
+          </el-table-column>
+          <el-table-column
+            prop="Rating"
+            label="Rating"
+            :width="ratingWidth"
+            >
+          </el-table-column>
+        </el-table>
     </div>
   </div>
   <div v-if="this.$store.state.user.globalVariable === 1">
     <!-- Selected Comments Section -->
     <div class="advice-layout">
       <div class="layout-title">Selected Comments</div>
-      <div class="content-box">
-        <div class="content-item" v-for="item in this.selectedComments" :key="item">
-            "{{ item }}"
-        </div>
-      </div>
+      <el-table
+          :data="selectedComments"
+          style="width: 100%"
+          border>
+          <el-table-column
+            prop="Comment"
+            label="Comment"
+            :width="commentWidth"
+            >
+          </el-table-column>
+          <el-table-column
+            prop="Rating"
+            label="Rating"
+            :width="ratingWidth"
+            >
+          </el-table-column>
+        </el-table>
     </div>
   </div>
   <div v-if = "this.$store.state.user.globalVariable === 1"> 
@@ -103,6 +127,8 @@
     data() {
       debugger
       return {
+        commentWidth: 0,
+        ratingWidth: 0,
         nameid:state => state.user.name,
         pickerOptions: {
           shortcuts: [{
@@ -127,7 +153,19 @@
       this.initOrderCountDate();
       this.getData();
     },
+    mounted() { // 用于动态调节Feedback和SelectedComments表列宽度
+      this.calculateColumnWidths();
+      window.addEventListener('resize', this.calculateColumnWidths); // 确保响应式
+    },
+    beforeDestroy() {
+      window.removeEventListener('resize', this.calculateColumnWidths); // 清理事件监听
+    },
     methods:{
+      calculateColumnWidths() {
+        const containerWidth = this.$el.querySelector('.advice-layout').clientWidth; // 或其他容器
+        this.commentWidth = containerWidth * 0.9; // 90% 宽
+        this.ratingWidth = containerWidth * 0.1; // 10% 宽
+      },
       handleDateChange(){
         this.getData();
       },
@@ -171,11 +209,22 @@
         }, 1000)
       },
       edit() {
-        const blob = new Blob([JSON.stringify({"UserName":this.$store.state.user.name,
-                                              "StoreName":this.storeName,
-                                              "StoreLocation":this.storeLocation})],
-                              {type: 'application/json'});
-        window.open(URL.createObjectURL(blob));
+        this.$confirm('Do you want to update your store info?', 'Tip', {
+          confirmButtonText: 'Confirm',
+          cancelButtonText: 'Cancel',
+          type: 'warning'
+        }).then(() => {
+          const blob = new Blob([JSON.stringify({"UserName":this.$store.state.user.name,
+                                                "StoreName":this.storeName,
+                                                "StoreLocation":this.storeLocation})],
+                                {type: 'application/json'});
+          window.open(URL.createObjectURL(blob));
+          this.$message({
+              message: 'Updated successfully',
+              type: 'success',
+              duration: 1000
+            });
+        });
       },
     }
   }
