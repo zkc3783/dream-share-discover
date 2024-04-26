d9975f31d1722ffdc2084abf95adfaa492e79e6d
echo <1440204228@qq.com>
2024-04-27 01:48:37 +0800

    only some specific in our interface

--- project/mall_vue/config/dev.env.js
+++ project/mall_vue/config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:3000"'
+  BASE_API: '"https://admin-api.macrozheng.com"'
 })
